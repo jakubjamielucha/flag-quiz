f6b5b4afaf53f043ea8026137757487c59cf1dce
jakubjamielucha <jakubjamielucha@gmail.com>
2023-11-20 01:03:06 +0100

    Adding all the flags and creating quiz activity.

--- app/src/main/java/com/example/flagquiz/MainActivity.java
+++ app/src/main/java/com/example/flagquiz/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.flagquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStart;
+    private Button buttonStart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewStart = findViewById(R.id.textViewStart);
+        buttonStart = findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
